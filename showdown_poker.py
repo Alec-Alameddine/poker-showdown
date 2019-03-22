2e16b5afb2e671efec4b93048bfba379f4025148
alecA <alec935@gmail.com>
2019-03-21 20:19:49 -0400

    Update showdown_poker.py

--- showdown_poker.py
+++ showdown_poker.py
@@ -11,8 +11,8 @@ class Card:
     def __init__ (self,value,suit):
         self.value = value
         self.suit = suit
-        self.vname = ''
-        self.sname = ''
+        self.vname = vname[value]
+        self.sname = sname[suit]
 
     def __str__(self):
         return f'{self.sname}{self.vname}{self.sname}'
@@ -22,43 +22,6 @@ class Card:
             return f'{self.value}{self.suit[0].lower()}'
         if self.value > 10:
             return f'{self.vname[0]}{self.suit[0].lower()}'
-
-    def vsname(self,value,suit):
-        if self.value == 2:
-            self.vname = 'Two'
-        elif self.value == 3:
-            self.vname = 'Three'
-        elif self.value == 4:
-            self.vname = 'Four'
-        elif self.value == 5:
-            self.vname = 'Five'
-        elif self.value == 6:
-            self.vname = 'Six'
-        elif self.value == 7:
-            self.vname = 'Seven'
-        elif self.value == 8:
-            self.vname = 'Eight'
-        elif self.value == 9:
-            self.vname = 'Nine'
-        elif self.value == 10:
-            self.vname = 'Ten'
-        elif self.value == 11:
-            self.vname = 'Jack'
-        elif self.value == 12:
-            self.vname = 'Queen'
-        elif self.value == 13:
-            self.vname = 'King'
-        elif self.value == 14:
-            self.vname = 'Ace'
-
-        if self.suit == "Hearts":
-            self.sname = '♥'
-        elif self.suit == "Spades":
-            self.sname = '♠'
-        elif self.suit == "Clubs":
-            self.sname = '♣'
-        elif self.suit == "Diamonds":
-            self.sname = '♦'
 
 #All Decks
 class Deck:
@@ -76,7 +39,6 @@ class Deck:
     def draw(self,x):
         for y in range(x):
             drawcards[y] = self.cards.pop()
-            drawcards[y].vsname(drawcards[y].value,drawcards[y].suit)
 
         return drawcards
 
@@ -190,38 +152,10 @@ def post_draw():
 
 
 #Evaluation Functions
-def valname(x):
-    if x == 2:
-        return 'Two'
-    elif x == 3:
-        return 'Three'
-    elif x == 4:
-        return 'Four'
-    elif x == 5:
-        return 'Five'
-    elif x == 6:
-        return 'Six'
-    elif x == 7:
-        return 'Seven'
-    elif x == 8:
-        return 'Eight'
-    elif x == 9:
-        return 'Nine'
-    elif x == 10:
-        return 'Ten'
-    elif x == 11:
-        return 'Jack'
-    elif x == 12:
-        return 'Queen'
-    elif x == 13:
-        return 'King'
-    elif x == 14 or x == 1:
-        return 'Ace'
-
 def hcard(values):
     global strength
     strength = BaseStrength.HIGH_CARD.value + 10*values[0] + values[1] + .1*values[2] + .01*values[3] + .001*values[4]
-    return f'High-Card {valname(values[0])}'
+    return f'High-Card {vname[values[0]]}'
 
 def numpair(values):
     global strength
@@ -233,14 +167,14 @@ def numpair(values):
         for _ in range(2):
             vp.remove(pairs[0])
         strength = BaseStrength.PAIR.value + 10*pairs[0] + vp[0] + .1*vp[1] + .01*vp[2];
-        return f'Pair of {valname(pairs[0])}s'
+        return f'Pair of {vname[pairs[0]]}s'
     if len(pairs) >= 2:
         vps = values.copy()
         pairs = sorted(pairs,reverse=True)
         for _ in range(2):
             vps.remove(pairs[0]); vps.remove(pairs[1])
         strength = (BaseStrength.TWO_PAIR.value + 10*int(pairs[0]) + int(pairs[1])) + .1*vps[0]
-        return f'{valname(pairs[0])}s and {valname(pairs[1])}s'
+        return f'{vname[pairs[0]]}s and {vname[pairs[1]]}s'
 
 
 def trip(values):
@@ -253,7 +187,7 @@ def trip(values):
         for _ in range(3):
             vs.remove(trips[0])
         strength = BaseStrength.SET.value + 10*trips[0] + vs[0] + .1*vs[1]
-        return f'Set of {valname(trips[0])}s'
+        return f'Set of {vname[trips[0]]}s'
 
 def straight(vset,get_vals=False):
     global strength
@@ -267,7 +201,7 @@ def straight(vset,get_vals=False):
                 max_c = rank
                 if count == 5:
                     strength = BaseStrength.STRAIGHT.value + 10*min(vset)
-                    straight = f'Straight from {valname(max_c-4)} to {valname(max_c)}'
+                    straight = f'Straight from {vname[max_c-4]} to {vname[max_c]}'
                     break
             else: count = 0
         return straight
@@ -293,7 +227,7 @@ def flush(suits,all_cards):
         return False
     else:
         strength = BaseStrength.FLUSH.value + 10*flushes_vals[0] + flushes_vals[1] + .1*flushes_vals[2] + .01*flushes_vals[3] + .001*flushes_vals[4]
-        flush = f'{valname(max(flushes_vals))}-High flush of {flushes[0]}'
+        flush = f'{vname[max(flushes_vals)]}-High flush of {flushes[0]}'
     return flush
 
 def fullhouse(values):
@@ -306,16 +240,16 @@ def fullhouse(values):
 
     if pairs and trips:
         strength = BaseStrength.FULL_HOUSE.value + 10*trips[0] + pairs[0]
-        fh = f'{valname(trips[0])}s full of {valname(pairs[0])}s'
+        fh = f'{vname[trips[0]]}s full of {vname[pairs[0]]}s'
 
     if len(trips) > 1:
         if pairs:
             if trips[1] > pairs[0]:
                 strength = BaseStrength.FULL_HOUSE.value + 10*trips[0] + trips[1]
-                fh = f'{valname(trips[0])}s full of {valname(trips[1])}s'
+                fh = f'{vname[trips[0]]}s full of {vname[trips[1]]}s'
         else:
             strength = BaseStrength.FULL_HOUSE.value + 10*trips[0] + trips[1]
-            fh = f'{valname(trips[0])}s full of {valname(trips[1])}s'
+            fh = f'{vname[trips[0]]}s full of {vname[trips[1]]}s'
 
     if len(trips) == 1 and not pairs:
         return False
@@ -331,7 +265,7 @@ def quads(values):
         vq = values.copy()
         for _ in range(4): vq.remove(quads[0])
         strength = BaseStrength.QUADS.value + 10*quads[0] + vq[0]
-        return f'Quad {valname(quads[0])}s'
+        return f'Quad {vname[quads[0]]}s'
 
 def straightflush(suits,vset,all_cards):
     global strength
@@ -349,7 +283,7 @@ def straightflush(suits,vset,all_cards):
 
     if straight_ == "Normal":
         strength = BaseStrength.STRAIGHT_FLUSH.value + 10*max(flushes_vals)
-        sf = f'{valname(max(straight_vals))}-High Straight Flush of {flushes[0]}'
+        sf = f'{vname[max(straight_vals)]}-High Straight Flush of {flushes[0]}'
     elif straight_ == "Wheel":
         strength = BaseStrength.STRAIGHT_FLUSH.value
         sf = f'Five-High Straight Flush of {flushes[0]}'
@@ -384,9 +318,27 @@ def count_hand_occurence(strength):
     elif strength < 10000: hand_occurence[8]+=1
     elif strength == 10000: hand_occurence[9]+=1
 
+#Main Function
+def showdown_poker():
+    for h_inc in range(hnumber): #Hand Print Loop
+        user_hand = deck.draw(cards_per_hand)
+        print_hand(user_hand)
+
+        values,vset,suits,all_cards = determine(user_hand)
+        exact_hand = evalhand(values,suits,vset,all_cards)
+        print('\n'+exact_hand,end=" "); ss()
+
+        count_hand_occurence(strength)
+        h_strength[h_inc] = strength
+
+    post_draw()
+
 
 hand_occurence = {0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
 ho_names = ['High Card: ','Pair: ','Two-Pair: ','Three of a Kind: ','Straight: ','Flush: ','Full House: ','Four of a Kind: ','Straight Flush: ','Royal Flush: ']
+
+vname = {1: 'Ace', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Jack', 12: 'Queen', 13: 'King', 14: 'Ace'}
+sname = {"Hearts": '♥', "Spades": '♠', "Clubs": '♣', "Diamonds": '♦'}
 
 drawcards, h_strength = {}, {}
 
@@ -396,16 +348,7 @@ deck_start_time = time()
 deck = Deck()
 deck_end_time = time()
 
-#Hand Print Loop
-for h_inc in range(hnumber):
-    user_hand = deck.draw(cards_per_hand)
-    print_hand(user_hand)
 
-    values,vset,suits,all_cards = determine(user_hand)
-    exact_hand = evalhand(values,suits,vset,all_cards)
-    print('\n'+exact_hand,end=" "); ss()
 
-    count_hand_occurence(strength)
-    h_strength[h_inc] = strength
-
-post_draw()
+if __name__ == '__main__':
+    showdown_poker()
